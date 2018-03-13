523d96644428fe3315cdbecc8192e6ff9dae0ebc
Ironjanowar <ironjanowar@gmail.com>
2018-03-13 20:27:34 +0100

    Ratings now!

--- lib/refraner.ex
+++ lib/refraner.ex
@@ -1,5 +1,5 @@
 defmodule Refraner do
-  import Refraner.Utils, only: [id_to_integer: 1]
+  import Refraner.Utils
   import Ecto.Query, only: [from: 2]
 
   def get_random_refran() do
@@ -12,7 +12,11 @@ defmodule Refraner do
     Refraner.Model.Refran |> Refraner.Repo.get(id)
   end
 
-  def add_rating(_refran_id, _rate) do
-    # TODO
+  def add_rating(refran_id, new_rate) do
+    %{rate: rate, total_votos: total_votos} = refran = get_refran_by_id(refran_id)
+    {new_rate, new_total_votos} = calculate_new_rate(rate, total_votos, new_rate)
+
+    Refraner.Model.Refran.update_rate(refran, %{rate: new_rate, total_votos: new_total_votos})
+    |> Refraner.Repo.update()
   end
 end
