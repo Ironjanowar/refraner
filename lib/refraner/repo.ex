802a0f0fbf10ed2875e8ec2cb9a49ff63f0205c6
Cesar <cga@softtelecom.eu>
2018-04-25 14:35:13 +0200

    Trying to docker stuff

--- lib/refraner/repo.ex
+++
@@ -1,3 +0,0 @@
-defmodule Refraner.Repo do
-  use Ecto.Repo, otp_app: :refraner, adapter: Sqlite.Ecto2
-end
--- test/refraner_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RefranerTest do
-  use ExUnit.Case
-  doctest Refraner
-
-  test "greets the world" do
-    assert Refraner.hello() == :world
-  end
-end
